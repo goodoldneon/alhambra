67e34bc52ae33a8c209710fcb75d26cbdf56023f
aharper <aharper@altair.com>
2019-01-12 13:52:42 -0500

    Add alhambra.release()

--- sandbox/arrayOfObjects.js
+++ sandbox/arrayOfObjects.js
@@ -4,6 +4,12 @@ const arr = [{ a: 1 }, { a: 2 }, { a: 3 }];
 
 const p = alhambra.protect(arr);
 
-p[1].a = 100;
+p.push(1);
 
-console.log(arr, p, p.__copy);
+const newP = p.map((item) => item);
+
+newP[1].a = 100;
+
+console.log(arr);
+console.log(p);
+console.log(alhambra.release(p));
--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ const { ProxyFactory } = require('./ProxyFactory');
 
 module.exports = {
   protect: ProxyFactory,
+  release: (target) => {
+    if (typeof target !== 'object' || !target.__isProxy) {
+      throw new TypeError("Cannot release something that wasn't protected first");
+    }
+
+    return target.__copy;
+  },
 };
