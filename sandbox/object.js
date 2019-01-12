26107f3fe2b619060647ac00bebba46806118e9d
aharper <aharper@altair.com>
2019-01-12 12:59:27 -0500

    Recursive with callbacks

--- sandbox/object.js
+++ sandbox/object.js
@@ -15,6 +15,16 @@ const obj = {
 const p = alhambra.protect(obj);
 
 p.foo.bar.baz = 'bbb';
+// console.log(p.__isProxy);
 console.log(obj.foo.bar.baz, p.foo.bar.baz, p.__copy.foo.bar.baz);
 p.metadata.name = 'bar';
+// console.log(p.__isProxy);
 console.log(obj.metadata.name, p.metadata.name, p.__copy.metadata.name);
+p.id = 2;
+console.log(obj.id, p.id, p.__copy.id);
+
+
+// console.log('\n');
+// console.log(p);
+// console.log('\n');
+// console.log(p.__copy);
--- src/ProxyFactory.js
+++ src/ProxyFactory.js
@@ -1,51 +1,52 @@
 const { has, isArray, isObject } = require('lodash');
 
-const ProxyFactory = (original) => {
+const deepStripProxies = (target) => {
+  if (isObject(target)) {
+    if (target.__isProxy) {
+      const newTarget = target.__copy;
+
+      Object.entries(newTarget).forEach(([key, value]) => {
+        newTarget[key] = deepStripProxies(value);
+      });
+
+      return newTarget;
+    }
+  }
+
+  return target;
+};
+
+const ProxyFactory = (original, onChange = () => {}) => {
+  // console.log('');
+  // console.log(original);
   let copy;
   let handler;
   let isChanged = false;
 
-  const arrayHandler = {
-    get: function(target, key) {
-      if (key === '__copy') {
-        return isChanged ? copy : original;
-      }
-
-      if (has(target, key)) {
-        if (isObject(target[key])) {
-          traverser[key] = { ...target[key] };
-
-          traverser = traverser[key];
-
-          return new Proxy(traverser, objectHandler);
-        }
-
-        return target[key];
-      }
-
-      return Reflect.get(target, key) || Reflect.get(Array.prototype, key);
-    },
-    set: function(target, key, value) {
-      target[key] = value;
-      isChanged = true;
-
-      return true;
-    },
+  const handleChange = () => {
+    isChanged = true;
+    onChange();
   };
 
   const objectHandler = {
     get: function(target, key) {
       if (key === '__copy') {
-        return isChanged ? copy : original;
+        return isChanged ? deepStripProxies(copy) : original;
+      }
+
+      if (key === '__isChanged') {
+        return isChanged;
+      }
+
+      if (key === '__isProxy') {
+        return true;
       }
 
       if (has(target, key)) {
         if (isObject(target[key])) {
-          traverser[key] = { ...target[key] };
+          copy[key] = ProxyFactory(copy[key], handleChange);
 
-          traverser = traverser[key];
-
-          return new Proxy(traverser, objectHandler);
+          return copy[key];
         }
 
         return target[key];
@@ -56,20 +57,21 @@ const ProxyFactory = (original) => {
     set: function(target, key, value) {
       target[key] = value;
       isChanged = true;
+      onChange();
 
       return true;
     },
   };
 
   if (isArray(original)) {
-    copy = [...original];
-    handler = arrayHandler;
+    // copy = [...original];
+    // handler = arrayHandler;
   } else if (isObject(original)) {
     copy = { ...original };
     handler = objectHandler;
   }
 
-  let traverser = copy;
+  // let traverser = copy;
 
   return new Proxy(copy, handler);
 };
