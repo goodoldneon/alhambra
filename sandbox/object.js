01c1f99a237b66b7c9e437f5e17fe701bc5820a0
aharper <aharper@altair.com>
2019-01-12 13:39:03 -0500

    Add tests for arrays as object properties

--- sandbox/object.js
+++ sandbox/object.js
@@ -8,8 +8,10 @@ const obj = {
   foo: {
     bar: {
       baz: 'aaa',
+      items: [1, 2, 3],
     },
   },
+  items: [1, 2, 3],
 };
 
 const p = alhambra.protect(obj);
@@ -17,12 +19,12 @@ const p = alhambra.protect(obj);
 // p.foo.bar.baz = 'bbb';
 // console.log(obj.foo.bar.baz, p.foo.bar.baz, p.__copy.foo.bar.baz);
 // p.metadata.name = 'bar';
-delete p.metadata.name;
-console.log(obj.metadata.name, p.metadata.name, p.__copy.metadata.name, obj === p.__copy);
+// delete p.metadata.name;
+// console.log(obj.metadata.name, p.metadata.name, p.__copy.metadata.name, obj === p.__copy);
 // p.id = 2;
 // console.log(obj.id, p.id, p.__copy.id);
-
-// console.log('\n');
-// console.log(p);
-// console.log('\n');
-// console.log(p.__copy);
+// p.items.push(100);
+// console.log(obj.items, p.items, p.__copy.items);
+p.items.push(100);
+console.log(obj.items, p.items, p.__copy.items);
+// console.log(obj.foo.bar.items, p.foo.bar.items, p.__copy.foo.bar.items);
--- tests/Object.test.js
+++ tests/Object.test.js
@@ -13,8 +13,10 @@ describe('Object', () => {
       foo: {
         bar: {
           baz: 'aaa',
+          arr: [1, 2, 3],
         },
       },
+      arr: [1, 2, 3],
     };
 
     p = ProxyFactory(obj);
@@ -27,94 +29,160 @@ describe('Object', () => {
   });
 
   describe('is created when', () => {
-    it('property is changed', () => {
-      p.id = 2;
-      expect(obj === p.__copy).toBe(false);
+    describe('primitive', () => {
+      it('property is changed', () => {
+        p.id = 2;
+        expect(obj === p.__copy).toBe(false);
+      });
+
+      it('nested property is changed', () => {
+        p.metadata.name = 'bar';
+        expect(obj === p.__copy).toBe(false);
+      });
+
+      it('deeply nested property is changed', () => {
+        p.foo.bar.baz = 'bbb';
+        expect(obj === p.__copy).toBe(false);
+      });
+
+      it('delete operator is used', () => {
+        delete p.foo.bar.baz;
+        expect(obj === p.__copy).toBe(false);
+      });
     });
 
-    it('nested property is changed', () => {
-      p.metadata.name = 'bar';
-      expect(obj === p.__copy).toBe(false);
-    });
+    describe('array', () => {
+      it('index is changed', () => {
+        p.arr[1] = 100;
+        expect(obj === p.__copy).toBe(false);
+      });
 
-    it('deeply nested property is changed', () => {
-      p.foo.bar.baz = 'bbb';
-      expect(obj === p.__copy).toBe(false);
-    });
+      it('deeply nested index is changed', () => {
+        p.foo.bar.arr[1] = 100;
+        expect(obj === p.__copy).toBe(false);
+      });
 
-    it('delete operator is used', () => {
-      delete p.foo.bar.baz;
-      expect(obj === p.__copy).toBe(false);
+      it('deeply nested push()', () => {
+        p.foo.bar.arr.push();
+        expect(obj === p.__copy).toBe(false);
+      });
     });
   });
 
   describe('mutate', () => {
     describe('does not change original when', () => {
-      it('property is changed', () => {
-        p.id = 2;
-        expect(obj.id).toBe(1);
+      describe('primitive', () => {
+        it('property is changed', () => {
+          p.id = 2;
+          expect(obj.id).toBe(1);
+        });
+
+        it('nested property is changed', () => {
+          p.metadata.name = 'bar';
+          expect(obj.metadata.name).toBe('foo');
+        });
+
+        it('deeply nested property is changed', () => {
+          p.foo.bar.baz = 'bbb';
+          expect(obj.foo.bar.baz).toBe('aaa');
+        });
+
+        it('multiple depths are changed', () => {
+          p.foo.bar.baz = 'bbb';
+          expect(obj.foo.bar.baz).toBe('aaa');
+          p.metadata.name = 'bar';
+          expect(obj.metadata.name).toBe('foo');
+          p.id = 2;
+          expect(obj.id).toBe(1);
+        });
+
+        it('delete operator is used', () => {
+          delete p.foo.bar.baz;
+          expect(obj.foo.bar.baz).toBe('aaa');
+        });
       });
 
-      it('nested property is changed', () => {
-        p.metadata.name = 'bar';
-        expect(obj.metadata.name).toBe('foo');
-      });
+      describe('array', () => {
+        it('index is changed', () => {
+          p.arr[1] = 100;
+          expect(obj.arr[1]).toBe(2);
+        });
 
-      it('deeply nested property is changed', () => {
-        p.foo.bar.baz = 'bbb';
-        expect(obj.foo.bar.baz).toBe('aaa');
-      });
+        it('deeply nested index is changed', () => {
+          p.foo.bar.arr[1] = 100;
+          expect(obj.foo.bar.arr[1]).toBe(2);
+        });
 
-      it('multiple depths are changed', () => {
-        p.foo.bar.baz = 'bbb';
-        expect(obj.foo.bar.baz).toBe('aaa');
-        p.metadata.name = 'bar';
-        expect(obj.metadata.name).toBe('foo');
-        p.id = 2;
-        expect(obj.id).toBe(1);
-      });
-
-      it('delete operator is used', () => {
-        delete p.foo.bar.baz;
-        expect(obj.foo.bar.baz).toBe('aaa');
+        it('deeply nested push()', () => {
+          p.foo.bar.arr.push();
+          expect(obj.foo.bar.arr.length).toBe(3);
+        });
       });
     });
 
     describe('changes new object when', () => {
-      it('property is changed', () => {
-        p.id = 2;
-        expect(p.id).toBe(2);
-        expect(p.__copy.id).toBe(2);
+      describe('primitive', () => {
+        it('property is changed', () => {
+          p.id = 2;
+          expect(p.id).toBe(2);
+          expect(p.__copy.id).toBe(2);
+        });
+
+        it('nested property is changed', () => {
+          p.metadata.name = 'bar';
+          expect(p.metadata.name).toBe('bar');
+          expect(p.__copy.metadata.name).toBe('bar');
+        });
+
+        it('deeply nested property is changed', () => {
+          p.foo.bar.baz = 'bbb';
+          expect(p.foo.bar.baz).toBe('bbb');
+          expect(p.__copy.foo.bar.baz).toBe('bbb');
+        });
+
+        it('multiple depths are changed', () => {
+          p.foo.bar.baz = 'bbb';
+          expect(p.foo.bar.baz).toBe('bbb');
+          expect(p.__copy.foo.bar.baz).toBe('bbb');
+          p.metadata.name = 'bar';
+          expect(p.metadata.name).toBe('bar');
+          expect(p.__copy.metadata.name).toBe('bar');
+          p.id = 2;
+          expect(p.id).toBe(2);
+          expect(p.__copy.id).toBe(2);
+        });
+
+        it('delete operator is used', () => {
+          delete p.foo.bar.baz;
+          expect(p.foo.bar.baz).toBe(undefined);
+          expect(p.__copy.foo.bar.baz).toBe(undefined);
+        });
       });
 
-      it('nested property is changed', () => {
-        p.metadata.name = 'bar';
-        expect(p.metadata.name).toBe('bar');
-        expect(p.__copy.metadata.name).toBe('bar');
-      });
+      describe('array', () => {
+        it('index is changed', () => {
+          p.arr[1] = 100;
+          expect(p.arr[1]).toBe(100);
+          expect(p.__copy.arr[1]).toBe(100);
+        });
 
-      it('deeply nested property is changed', () => {
-        p.foo.bar.baz = 'bbb';
-        expect(p.foo.bar.baz).toBe('bbb');
-        expect(p.__copy.foo.bar.baz).toBe('bbb');
-      });
+        it('deeply nested index is changed', () => {
+          p.foo.bar.arr[1] = 100;
+          expect(p.foo.bar.arr[1]).toBe(100);
+          expect(p.__copy.foo.bar.arr[1]).toBe(100);
+        });
 
-      it('multiple depths are changed', () => {
-        p.foo.bar.baz = 'bbb';
-        expect(p.foo.bar.baz).toBe('bbb');
-        expect(p.__copy.foo.bar.baz).toBe('bbb');
-        p.metadata.name = 'bar';
-        expect(p.metadata.name).toBe('bar');
-        expect(p.__copy.metadata.name).toBe('bar');
-        p.id = 2;
-        expect(p.id).toBe(2);
-        expect(p.__copy.id).toBe(2);
-      });
+        it('push()', () => {
+          p.arr.push(100);
+          expect(p.arr.length).toBe(4);
+          expect(p.__copy.arr.length).toBe(4);
+        });
 
-      it('delete operator is used', () => {
-        delete p.foo.bar.baz;
-        expect(p.foo.bar.baz).toBe(undefined);
-        expect(p.__copy.foo.bar.baz).toBe(undefined);
+        it('deeply nested push()', () => {
+          p.foo.bar.arr.push(100);
+          expect(p.foo.bar.arr.length).toBe(4);
+          expect(p.__copy.foo.bar.arr.length).toBe(4);
+        });
       });
     });
   });
